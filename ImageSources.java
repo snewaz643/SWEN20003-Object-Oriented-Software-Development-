8a64f08ffd10dc34423269140c1fc73c065c54b2
S.A. Newaz Saif <snewaz@student.unimelb.edu.au>
2018-09-15 02:51:09 +1000

    add src folder

---
+++ ImageSources.java
@@ -0,0 +1,7 @@
+public abstract class ImageSources
+{
+	public static final String BUS = "assets/bus.png";
+	public static final String FROG = "assets/frog.png";
+	public static final String GRASS = "assets/grass.png";
+	public static final String WATER = "assets/water.png";
+}
