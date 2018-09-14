943b572faa0348698fcabfd877ce5c0840b99e80
S.A. Newaz Saif <snewaz@student.unimelb.edu.au>
2018-09-15 02:55:35 +1000

    Delete ImageSources.java

--- ImageSources.java
+++
@@ -1,7 +0,0 @@
-public abstract class ImageSources
-{
-	public static final String BUS = "assets/bus.png";
-	public static final String FROG = "assets/frog.png";
-	public static final String GRASS = "assets/grass.png";
-	public static final String WATER = "assets/water.png";
-}
